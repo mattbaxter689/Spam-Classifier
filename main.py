0184b5626e0a1ce1d9c89c7b7198cd5f7320bd1f
Matthew Baxter <matthewbaxter0412@hotmail.com>
2026-01-05 10:50:32 -0500

    Adding in custom pytorch dataset to handle tokenizing data

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from spam.data.utils import create_datasets_from_dataframe
 import pandas as pd
-import os
 
 
 def main():
     data = pd.read_csv("dataset/Enron.csv")
-    print(data)
+    train, val, test = create_datasets_from_dataframe(data)
+
+    for val in train:
+        print(val)
 
 
 if __name__ == "__main__":

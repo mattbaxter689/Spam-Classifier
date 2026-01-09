6277406cb542e69c7d4369b2c6340ac01205eb64
Matthew Baxter <matthewbaxter0412@hotmail.com>
2026-01-09 11:57:45 -0500

    Created factory for reusability of components across

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 # TODO: This is the main file for testing at the moment. It will change as time goes on
 import mlflow
+from transformers import AutoTokenizer, AutoModel
 from spam.data.utils import create_datasets_from_dataframe
 from spam.training.trainer import TrainingManager
 from spam.registry.register import ChampionChallengerManager
 import pandas as pd
-import os
+import torch.nn as nn
+
+
+def encoder_factory() -> nn.Module:
+    return AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
 
 def main():
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
     # mlflow.set_experiment("spam-classifier")
     data = pd.read_csv("dataset/Enron.csv")
-    train, val, test = create_datasets_from_dataframe(data)
 
-    manager = TrainingManager(train, val, test)
+    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
+    train, val, test = create_datasets_from_dataframe(data, tokenizer)
+
+    manager = TrainingManager(encoder_factory, train, val, test)
     manager.tune()
     recall = manager.train_final()
     post_fit_manager = ChampionChallengerManager()
